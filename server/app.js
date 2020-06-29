7a5f491067a3ca95f2ae46b0371aee3f5c9ac5dc
Mauro <matprogrammer@gmail.com>
2020-06-28 22:56:02 -0300

    cors

--- server/app.js
+++ server/app.js
@@ -23,11 +23,14 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/', indexRoutes);
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+app.use(cors());
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
-app.use(cors());
 
 app.listen(app.get('port'), () => {
     console.log(`Server on port ${app.get('port')}`);
